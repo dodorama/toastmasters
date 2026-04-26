721ae092b289da2f54b0718433aeb1a721d6dcb9
Paolo Agostinelli <dodo@192.168.1.18>
2026-04-25 21:44:59 -0600

    Initial commit

---
+++ scripts/popupconfig.js
@@ -0,0 +1,12 @@
+var popupLinkConfig = new Array;
+// Delete/copy/modify the following lines to configure your popup windows.
+popupLinkConfig["popup"] = new Array ( "", "width=500,height=600,scrollbar=yes,menubar=yes");
+popupLinkConfig["gallery330"] = new Array ( "", "width=380,height=500,scrollbar=yes,menubar=yes");
+popupLinkConfig["gallery450"] = new Array ( "", "width=500,height=380,scrollbar=yes,menubar=yes");
+popupLinkConfig["gallery660"] = new Array ( "", "width=710,height=380,scrollbar=yes,menubar=yes");
+popupLinkConfig["gallery600"] = new Array ( "", "width=530,height=730,scrollbar=yes,menubar=yes");
+popupLinkConfig["food"] = new Array ( "", "width=510,height=288,scrollbar=yes,menubar=yes");
+popupLinkConfig["glueing"] = new Array ( "", "width=362,height=345,scrollbar=yes,menubar=yes");
+popupLinkConfig["spring"] = new Array ( "", "width=342,height=275,scrollbar=yes,menubar=yes");
+popupLinkConfig["all"] = new Array ( "", "width=350,height=288,scrollbar=yes,menubar=yes");
+popupLinkConfig["player"] = new Array ( "", "width=212,height=364,scrollbar=yes,menubar=yes");
